07cb5e28be6e9d49346b27cf507faf6894499548
Nikolaj Lübker <nikolaj.luebker@lego.com>
2022-05-24 21:08:04 +0200

    Parse lang_target file + update debug

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::fs;
+use pest::Parser;
+
 extern crate pest;
 #[macro_use]
 extern crate pest_derive;
@@ -7,5 +10,12 @@ extern crate pest_derive;
 pub struct LIBParser;
 
 fn main() {
-    println!("Hello, world!");
+    // get file
+    let unparsed_file = fs::read_to_string("lang_target.lib")
+        .expect("cannot read file.");
+
+    // use parser
+    let file = LIBParser::parse(Rule::main, &unparsed_file)
+        .expect("cannot parse file.")
+        .next().unwrap();
 }
---
+++ target/debug/deps/lib_lang-36651b5a6684a568.d
@@ -0,0 +1,6 @@
+/Users/dkNikLue/dev/rs/large/lib_lang/target/debug/deps/lib_lang-36651b5a6684a568: src/main.rs /Users/dkNikLue/dev/rs/large/lib_lang/src/lib.pest
+
+/Users/dkNikLue/dev/rs/large/lib_lang/target/debug/deps/lib_lang-36651b5a6684a568.d: src/main.rs /Users/dkNikLue/dev/rs/large/lib_lang/src/lib.pest
+
+src/main.rs:
+/Users/dkNikLue/dev/rs/large/lib_lang/src/lib.pest:
