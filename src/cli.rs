59a00418eabf91fb7321b0dee8400a3625e787f6
Nikolaj Lübker <nikolaj.luebker@lego.com>
2022-05-26 22:41:54 +0200

    Added cli

---
+++ src/cli.rs
@@ -0,0 +1,51 @@
+use std::path::PathBuf;
+use std::fs;
+
+use clap::{Args, Parser, Subcommand};
+
+#[derive(Debug, Parser)]
+#[clap(name = "lib_lang")]
+#[clap(bin_name = "lib_lang")]
+#[clap(about = "dwmadwa", long_about = None)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    #[clap(arg_required_else_help = true)]
+    Run {
+        #[clap(long, parse(from_os_str))]
+        path: PathBuf,
+    },
+
+    #[clap(arg_required_else_help = true)]
+    Build {
+        #[clap(long, parse(from_os_str))]
+        path: PathBuf,
+    }
+}
+
+pub fn get_path() -> (bool, PathBuf) {
+    let args = Cli::parse();
+
+    let path = match args.command {
+        Commands::Build { path } => {
+            if path.extension().unwrap() != "lib" {
+                panic!("Wrong file extension!");
+            }
+
+            (false, path)
+        },
+        Commands::Run { path } => {
+            if path.extension().unwrap() != "lib" {
+                panic!("Wrong file extension!");
+            }
+
+            (true, path)
+        }
+    };
+
+    path
+}
