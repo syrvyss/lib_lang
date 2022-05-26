69f6029acc147e5504622e8e071faacac3038dc0
Nikolaj Lübker <nikolaj.luebker@lego.com>
2022-05-26 17:35:59 +0200

    fixed parse output

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::fs;
 use pest::Parser;
+use pest::error::Error;
 
 extern crate pest;
 #[macro_use]
@@ -40,101 +41,109 @@ fn main() {
 
     // use parser
     let file = LIBParser::parse(Rule::main, &unparsed_file)
-        .expect("cannot parse file.")
-        .next().unwrap();
+        .expect("cannot parse file.");
 
     let mut libraries: Vec<Library> = Vec::new();
 
-    for expression in file.into_inner() {
-        match expression.as_rule() {
-            Rule::init => {
-                let name = expression.into_inner().as_str();
-                libraries.push(Library {
-                    name: name.to_string(),
-                    users: Vec::new()
-                })
-            },
-
-            Rule::add => {
-                let mut inner_values = expression.into_inner();
-
-                let mut lib: Library = Library { name: String::new(), users: Vec::new() };
-                let mut type_name = Type::User;
-
-                for pair in inner_values {
-                    match pair.as_rule() {
-                        Rule::name_type => {
-                            let name = pair.into_inner().as_str();
-                        }, // library name
-                        Rule::types => {
-                            type_name = match pair.into_inner().as_str() {
-                                "user" => Type::User,
-                                "book" => Type::Book,
-                                _ => unreachable!()
-                            }
-                        }, // user, book
-                        Rule::brackets => {
-                            match type_name {
-                                Type::User => {
-                                    let user_name = &pair.into_inner()
-                                        .into_iter()
-                                        .filter(|x| x.as_rule() == Rule::name)
-                                        .next().unwrap().as_str().to_string();
-
-                                    libraries.iter_mut().next().unwrap().users.push(User {
-                                        name: user_name.to_string(), 
-                                        books: Vec::new()
-                                    });
+    for expression in file {
+        for test in expression.into_inner() {
+            match test.as_rule() {
+                Rule::init => {
+                    let name = test.into_inner().as_str();
+                    libraries.push(Library {
+                        name: name.to_string(),
+                        users: Vec::new()
+                    })
+                },
+    
+                Rule::add => {
+                    let mut inner_values = test.into_inner();
+    
+                    let mut lib: Library = Library { name: String::new(), users: Vec::new() };
+                    let mut type_name = Type::User;
+    
+                    for pair in inner_values {
+                        match pair.as_rule() {
+                            Rule::name_type => {
+                            }, // library name
+                            Rule::types => {
+                                type_name = match pair.as_str() {
+                                    "user" => Type::User,
+                                    "book" => Type::Book,
+                                    _ => unreachable!()
                                 }
-                                Type::Book => {
-                                    let mut index: usize;
-                                    let mut book_data: (String, String) = (String::new(), String::new());
-
-                                    match pair.as_rule() {
-                                        Rule::digit => index = pair.as_str().parse::<usize>().unwrap(),
-                                        Rule::title => book_data.0 = pair.as_str().to_string(),
-                                        Rule::author => book_data.1 = pair.as_str().to_string(),
-                                        _ => unreachable!()
+                            }, // user, book
+                            Rule::brackets => {
+                                match type_name {
+                                    Type::User => {
+                                        let user_name = &pair.into_inner()
+                                            .into_iter()
+                                            .filter(|x| x.as_rule() == Rule::name)
+                                            .next().unwrap().as_str().to_string();
+    
+                                        libraries.iter_mut().next().unwrap().users.push(User {
+                                            name: user_name.to_string(), 
+                                            books: Vec::new()
+                                        });
                                     }
+                                    Type::Book => {
+                                        let mut index = 1usize;
+                                        let mut book_data: (String, String) = (String::new(), String::new());
 
-                                    let index = pair.into_inner().as_str().parse::<usize>().unwrap();
-                                    lib.users.iter_mut().nth(index - 1 as usize).unwrap().books.push(Book {
-                                        title: book_data.0.to_string(),
-                                        author: book_data.1.to_string()
-                                    });
+                                        for item in pair.into_inner() {
+                                            match item.as_rule() {
+                                                Rule::index => index = item.into_inner().as_str().parse::<usize>().unwrap(),
+                                                Rule::title => book_data.0 = item.as_str().to_string(),
+                                                Rule::author => book_data.1 = item.as_str().to_string(),
+                                                _ => unreachable!()
+                                            }
+                                        }
 
-                                },
-                                _ => unreachable!()
-                            }
-                        },
-                        _ => unreachable!()
-                    }
-
-
-                }
-            },
-            Rule::print => {
-                match expression.as_rule() {
-                    Rule::name_type => {
-                        let name = expression.into_inner().next().unwrap().as_str();
-                    },
-                    Rule::types => {
-                        let type_name = expression.into_inner().next().unwrap().as_str();
-                        if type_name != "user" {
-                            panic!("Sorry, can't print book.");
+                                        libraries.iter_mut().next().unwrap().users.iter_mut().nth(index - 1usize).unwrap().books.push(Book {
+                                            title: book_data.0.to_string(),
+                                            author: book_data.1.to_string()
+                                        });
+    
+                                    },
+                                    _ => unreachable!()
+                                }
+                            },
+                            _ => unreachable!()
                         }
-                    },
-                    Rule::brackets => {
-                        let mut index: usize = expression.into_inner().next().unwrap().as_str().parse().unwrap();
-
-                        println!("Book {:?}", libraries.iter().next().unwrap().users.iter().nth(index - 1 as usize).unwrap());
-                    },
-                    _ => unreachable!()
-                }
-            },
-            _ => unreachable!()
+    
+    
+                    }
+                },
+                Rule::print => {
+                    for item in test.into_inner() {
+                        match item.as_rule() {
+                            Rule::name_type => {
+                                let name = item.into_inner().next().unwrap().as_str();
+                            },
+                            Rule::types => {
+                                let type_name = item.as_str();
+                                if type_name != "user" {
+                                    panic!("Sorry, can't print book.");
+                                }
+                            },
+                            Rule::brackets => {
+                                let index: usize = item.into_inner()
+                                    .as_str()
+                                    .replace("\n", "")
+                                    .split(" ")
+                                    .last().unwrap().parse().unwrap();
+        
+                                println!("Book {:?}", libraries.iter().next().unwrap().users.iter().nth(index - 1 as usize).unwrap());
+                            },
+                            _ => unreachable!()
+                        }
+                    }
+                },
+                _ => unreachable!()
+            }
         }
+        
     }
-    println!("Library: {:?}", libraries.iter().next());
+    println!("Library: {:?}", libraries.iter().next().unwrap());
     println!("Library user size: {:?}", libraries.iter().next().unwrap().users.iter().count());
 }
